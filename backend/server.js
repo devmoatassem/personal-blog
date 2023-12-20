51f5523e15dcf9d7c60eaafaea8810f78ddb31fe
devmoatassem <moatassemsindhu@gmail.com>
2023-12-20 23:00:57 +0500

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 import express from "express";
-import {regValidation, loginValidation} from "../backend/schema/formValidation.js";
-import userAuthValidation from "../backend/middlewares/userAuthValidation.js";
+import "dotenv/config.js";
+import connectDB from "./config/dbConnection.js";
 
+
+connectDB(); // connect to the database
 const server = new express();
 
 const PORT = 8000;
 
 server.use(express.json());
 
-server.post("/register",userAuthValidation(regValidation), (req,res)=>{
+// Use the user routes
+// server.use('/api/users', userRoutes);
 
-    res.send("Sign up route");
-    // const { fullname, email, password } = req.body;
-})
+// server.post("/register",userAuthValidation(regValidation), (req,res)=>{
 
-server.post("/login",userAuthValidation(loginValidation), (req,res)=>{
-    res.send("Login up route");
-})
+//     res.send("Sign up route");
+//     // const { fullname, email, password } = req.body;
+// })
+
+// server.post("/login",userAuthValidation(loginValidation), (req,res)=>{
+//     res.send("Login up route");
+// })
 
 server.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
