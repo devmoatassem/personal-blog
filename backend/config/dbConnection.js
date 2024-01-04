2256925036d0101b837dcf6c819635744d4528e4
devmoatassem <moatassemsindhu@gmail.com>
2024-01-04 18:30:24 +0500

    DB auto Indexing

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}:${conn.connection.port}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      autoIndex: true,
+    });
+    console.log(
+      `MongoDB connected: ${conn.connection.host}:${conn.connection.port}`
+    );
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
