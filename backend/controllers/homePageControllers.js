edf5bd1326372219b67cd6ec5b67c5871c7993c9
devmoatassem <moatassemsindhu@gmail.com>
2024-01-30 08:59:35 +0500

    latest blog route done

--- backend/controllers/homePageControllers.js
+++ backend/controllers/homePageControllers.js
@@ -22,4 +22,28 @@ const getLatesBlogs = async (req, res) => {
     });
 };
 
-export { getLatesBlogs };
+const getTrendingBlogs = async (req, res) => {
+  Blog.find({ draft: false })
+    .sort({
+      "activity.total_reads": -1,
+      "activity.total_likes": -1,
+      publishedAt: -1,
+    })
+    .populate(
+      "author",
+      "personal_info.fullname personal_info.username personal_info.profile_img "
+    ) // by adding -_id I can remove the _id from the response
+    .select("blog_id title des tags banner activity publishedAt")
+    .limit(5)
+    .then((blogs) => {
+      res.status(200).json({
+        blogs,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Server Error",
+      });
+    });
+};
+export { getLatesBlogs, getTrendingBlogs };
--- backend/routes/getBlogRoutes.js
+++ backend/routes/getBlogRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getLatesBlogs } from "../controllers/homePageControllers.js";
+import {
+  getLatesBlogs,
+  getTrendingBlogs,
+} from "../controllers/homePageControllers.js";
 
 const router = express.Router(); // create a router object
 
-
 // parent route is /blogs
 router.get("/latest", getLatesBlogs);
-
+router.get("/trending", getTrendingBlogs);
 export default router;
